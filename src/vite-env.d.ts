7d269929a1a9be379cb26b188df6639e3a6ccc3f
zqily <albertolomeoss@gmail.com>
2025-11-22 19:40:18 +0700

    Added vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="vite/client" />
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
